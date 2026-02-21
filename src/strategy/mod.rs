0f9844d1aeb8ba78ae4e07b85213f3ff816da5ae
Rosnaky <ronakjpatelr22@gmail.com>
2026-02-21 00:16:25 -0500

    Remove cargo-husky pre-commit

--- src/strategy/mod.rs
+++ src/strategy/mod.rs
@@ -47,7 +47,10 @@ pub fn create_strategy(config: &StrategySettings) -> Box<dyn Strategy> {
             *lookback_period,
             *threshold,
         )),
-        StrategyParams::MeanReversion { window: _, std_devs: _ } => {
+        StrategyParams::MeanReversion {
+            window: _,
+            std_devs: _,
+        } => {
             todo!("Need to implement")
         }
         StrategyParams::Custom { .. } => {
