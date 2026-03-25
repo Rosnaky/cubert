abf4acb104295127c527b24a90d630b7913e5109
Ronak <98669069+Rosnaky@users.noreply.github.com>
2026-03-25 00:24:43 -0400

    Add GEM Blockset (#5)
    * Added gem-blockset and fix build
    
    * Update submodule
    
    * Add submodule step to CI
    
    * Add CUDA setup to CI
    
    * Upload valid artifact
    
    * Add separate hashing for each job
    
    * Add dynamic CUDA find
    
    * Update CUDA version
    
    * Dedupe artifact name
    
    * Upgrade CUDA for GCC

--- build.rs
+++ build.rs
@@ -1,11 +1,39 @@
+use std::env;
+use std::path::PathBuf;
+
 fn main() {
-    let libpath = format!("{}/model", env!("CARGO_MANIFEST_DIR"));
+    let manifest_dir = PathBuf::from(env::var("CARGO_MANIFEST_DIR").unwrap());
+    let gem_dir = manifest_dir.join("dependencies/gem-blockset");
 
-    println!("cargo:rustc-link-search=native={}", libpath);
+    let cuda_path = env::var("CUDA_PATH")
+        .or_else(|_| env::var("CUDA_HOME"))
+        .unwrap_or_else(|_| "/opt/cuda".to_string());
 
-    println!("cargo:rustc-link-lib=cudastats");
+    let dst = cmake::Config::new(&gem_dir)
+        .define("CMAKE_CUDA_ARCHITECTURES", "75")
+        .define("CMAKE_CUDA_COMPILER", format!("{}/bin/nvcc", cuda_path))
+        .define("GEM_BUILD_FFI", "ON")
+        .build_target("gem_blockset")
+        .build();
 
-    println!("cargo:rustc-link-arg=-Wl,-rpath,{}", libpath);
+    println!("cargo:rustc-link-search=native={}/build", dst.display());
+    println!("cargo:rustc-link-lib=static=gem_blockset");
 
-    println!("cargo:rerun-if-changed=model/libcudastats.so");
+    println!("cargo:rustc-link-search=native={}/lib64", cuda_path);
+    println!("cargo:rustc-link-search=native={}/lib", cuda_path);
+    println!("cargo:rustc-link-lib=dylib=cudart");
+
+    println!("cargo:rustc-link-lib=dylib=stdc++");
+
+    let bindings = bindgen::Builder::default()
+        .header(gem_dir.join("models/ffi.h").to_str().unwrap())
+        .generate()
+        .expect("failed to generate bindings");
+
+    let out_path = PathBuf::from(env::var("OUT_DIR").unwrap());
+    bindings
+        .write_to_file(out_path.join("gem_bindings.rs"))
+        .expect("failed to write bindings");
+
+    println!("cargo:rerun-if-changed=dependencies/gem-blockset/models");
 }
