57527f7ecf7d6698b1273fc081f2bda70f214a0e
Rosnaky <ronakjpatelr22@gmail.com>
2026-03-24 23:49:49 -0400

    Add dynamic CUDA find

--- build.rs
+++ build.rs
@@ -5,9 +5,13 @@ fn main() {
     let manifest_dir = PathBuf::from(env::var("CARGO_MANIFEST_DIR").unwrap());
     let gem_dir = manifest_dir.join("dependencies/gem-blockset");
 
+    let cuda_path = env::var("CUDA_PATH")
+        .or_else(|_| env::var("CUDA_HOME"))
+        .unwrap_or_else(|_| "/opt/cuda".to_string());
+
     let dst = cmake::Config::new(&gem_dir)
         .define("CMAKE_CUDA_ARCHITECTURES", "75")
-        .define("CMAKE_CUDA_COMPILER", "/opt/cuda/bin/nvcc")
+        .define("CMAKE_CUDA_COMPILER", format!("{}/bin/nvcc", cuda_path))
         .define("GEM_BUILD_FFI", "ON")
         .build_target("gem_blockset")
         .build();
@@ -15,12 +19,12 @@ fn main() {
     println!("cargo:rustc-link-search=native={}/build", dst.display());
     println!("cargo:rustc-link-lib=static=gem_blockset");
 
-    println!("cargo:rustc-link-search=native=/opt/cuda/lib64");
+    println!("cargo:rustc-link-search=native={}/lib64", cuda_path);
+    println!("cargo:rustc-link-search=native={}/lib", cuda_path);
     println!("cargo:rustc-link-lib=dylib=cudart");
 
     println!("cargo:rustc-link-lib=dylib=stdc++");
 
-    // Generate gem-blockset FFI Rust bindings
     let bindings = bindgen::Builder::default()
         .header(gem_dir.join("models/ffi.h").to_str().unwrap())
         .generate()
